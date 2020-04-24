5f4cdb9d895a05c29ed32da9cec986680109d66d
Lucas Werkmeister <mail@lucaswerkmeister.de>
2020-04-24 13:02:25 +0200

    Automatically start a timer
    The host (or anyone) can start a round with “-round”, and the bot will
    ping everyone that it’s about to start, and then the next purely numeric
    message will start the countdown.
    
    State is tracked via a global for now, which isn’t very pretty, but I
    didn’t see any convenient helpers in the discord lib.

--- bot.py
+++ bot.py
@@ -5,6 +5,17 @@ token = open(".token").read()
 
 client = discord.Client()
 
+open_rounds = {} # dict from (guild_name, channel_name) to bool
+
+async def start_countdown(channel, minutes):
+    countdown = await channel.send("Time left: %d minutes" % minutes)
+    while minutes > 0:
+        await asyncio.sleep(60) # TODO timer skew from also awaiting countdown.edit() below
+        minutes -= 1
+        await countdown.edit(content="Time left: %d minutes" % minutes)
+    await countdown.edit(content="Time’s up!")
+    await channel.send("Time’s up!")
+
 @client.event
 async def on_ready():
     guild = discord.utils.get(client.guilds, name="Auregann's salon")
@@ -13,9 +24,12 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    global open_rounds
+    guild = message.guild
+    channel = message.channel
+    round_marker = (guild.name, channel.name)
     if message.author == client.user:
         return
-    channel = message.channel
     if channel.name != "tests":
         return
     if message.content.startswith("-timer "):
@@ -25,13 +39,13 @@ async def on_message(message):
         except ValueError:
             await channel.send("Invalid duration (in minutes)!")
             return
-        countdown = await channel.send("Time left: %d minutes" % minutes)
-        while minutes > 0:
-            await asyncio.sleep(60) # TODO timer skew from also awaiting countdown.edit() below
-            minutes -= 1
-            await countdown.edit(content="Time left: %d minutes" % minutes)
-        await countdown.edit(content="Time’s up!")
-        await channel.send("Time’s up!")
+        await start_countdown(channel, minutes)
+    if message.content == "-round":
+        await channel.send("@here New round!")
+        open_rounds[round_marker] = True
+    if message.content.isnumeric() and open_rounds.get(round_marker, False):
+        del open_rounds[round_marker]
+        await start_countdown(channel, minutes=1)
     # await channel.send(message.content)
 
 client.run(token)
