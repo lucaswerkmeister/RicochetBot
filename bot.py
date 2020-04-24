3999ee802f06b2ba31a97f3212a3f992e8f011e1
Lucas Werkmeister <mail@lucaswerkmeister.de>
2020-04-24 12:28:01 +0200

    Working timer
    Reacts to messages of the format “-timer {minutes}”.

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import asyncio
 import discord
 
 token = open(".token").read()
@@ -8,8 +9,26 @@ client = discord.Client()
 async def on_ready():
     guild = discord.utils.get(client.guilds, name="Auregann's salon")
     channel = discord.utils.get(guild.channels, name='tests')
-    await channel.send("Hello, World!")
-    print(dir(channel))
+    # await channel.send("Hello, World!")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    channel = message.channel
+    if channel.name != "tests":
+        return
+    if message.content.startswith("-timer "):
+        arg = message.content[len("-timer "):]
+        try:
+            minutes = int(arg)
+        except ValueError:
+            await channel.send("Invalid duration (in minutes)!")
+            return
+        await channel.send("Will wait for %d minutes" % minutes)
+        await asyncio.sleep(minutes * 60)
+        await channel.send("Time’s up!")
+    # await channel.send(message.content)
 
 client.run(token)
 
